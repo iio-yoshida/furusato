946746063c09ab3134fe6118cf907dc2c996a7ab
iio-yoshida <68998658+iio-yoshida@users.noreply.github.com>
2023-04-17 15:17:03 +0900

    TOPページ ver2.5：Youtube部分のhtml,CSS,JS構築

--- assets/js/top.js
+++ assets/js/top.js
@@ -18,4 +18,23 @@ document.addEventListener('DOMContentLoaded', function() {
     }
   }
   window.addEventListener('scroll', headerToggle);
+
+
+  const movieBtn = document.querySelector('.js-movieBtn');
+  const movieThumb = document.querySelector('.js-movieThumb');
+  const movie = document.querySelector('.js-movie');
+
+  // ボタンまたはサムネイルがクリックされたら、再生開始
+  movieBtn.addEventListener('click', playMovie);
+  movieThumb.addEventListener('click', playMovie);
+
+  function playMovie() {
+    // ボタンとサムネイルを非表示にする
+    movieBtn.classList.add('is-hidden');
+    movieThumb.classList.add('is-hidden');
+    movie.classList.add('is-active');
+
+    // 動画を再生する
+    movie.src += '?autoplay=1&mute=1';
+  }
 });
